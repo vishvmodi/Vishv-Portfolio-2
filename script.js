a80f0401c554eb96d68b1264bd2d8c6df9f5d003
Vishv Playz <mvishv6@gmail.com>
2026-04-25 00:26:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+lucide.createIcons();
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
